107b0c26b1ce61aae002ac8e27860075663ff528
Nikolai <Pohodenko>
2016-07-10 01:10:51 +0300

    prepare for DAO implementation. Re-Add entity.

---
+++ src/main/java/craft/spring_demo/dao/IDao.java
@@ -0,0 +1,13 @@
+package craft.spring_demo.dao;
+
+
+import java.util.List;
+
+/**
+ * Created by phdnk on 10-Jul-16.
+ */
+public interface IDao<E> {
+    void write(E record);
+    E load(int id);
+    List<E> loadAll(int id);
+}
---
+++ src/main/java/craft/spring_demo/dao/RecordEntityDao.java
@@ -0,0 +1,23 @@
+package craft.spring_demo.dao;
+
+import craft.spring_demo.model.RecordEntity;
+
+import java.util.List;
+
+/**
+ * Created by phdnk on 10-Jul-16.
+ */
+public class RecordEntityDao implements IDao<RecordEntity>
+{
+    public void write(RecordEntity record) {
+
+    }
+
+    public RecordEntity load(int id) {
+        return null;
+    }
+
+    public List<RecordEntity> loadAll(int id) {
+        return null;
+    }
+}
